4eb208946d52b36e73c9aad0e6c8a5e9227bbbbd
unknown <mohd.ammarm>
2020-11-18 21:26:59 +0530

    created properties details page

---
+++ prop_details.php
@@ -0,0 +1,92 @@
+<html>
+<head>
+    <link rel="stylesheet" href="prop_details.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar sticky-top navbar-expand-md navbar-light bg-light">
+  		<img src="asset/Logo.svg">
+  		<button class="navbar-toggler" data-toggle="collapse" data-target="#navlinks" aria-label="Togglenavigation">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+		<div class="collapse navbar-collapse" id="navlinks">
+    		<ul class="navbar-nav ml-auto">
+      			<li class="nav-item">
+        			<a class="nav-link" href="home.php" style="margin-right: 1.2rem; font-weight: bold;">HOME</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="solution.html" style="margin-right: 1.2rem; font-weight: bold;">HOW IT WORKS</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="properties.php" style="margin-right: 1.2rem; font-weight: bold;">PROPERTIES</a>
+				</li>
+				<li class="nav-item">
+        			<a class="nav-link" href="contact.html" style="margin-right: 1.2rem; font-weight: bold;">CONTACT US</a>
+				</li>
+				<?php if(isset($_COOKIE['EMAIL'])) : ?>
+					<li class="nav-item">
+						<a href="profile.php" class="nav-link" style="margin-right: 1.2rem; font-weight: bold;"><?php echo $_COOKIE['EMAIL'];?></a>
+					</li>
+					<li class="nav-item">
+						<a href="delete.php" class="btn btn-primary" style="background-color: #12213F!important;">LOGOUT</a>
+					</li>
+				<?php else : ?>
+					<li class="nav-item">
+						<a href="login.php" class="btn btn-primary" style="margin-right: 0.75rem;background-color: #12213F!important;">LOGIN</a>
+					</li>
+					<li class="nav-item">
+						<a href="form.php" class="btn btn-primary" style="background-color: #12213F!important;">SIGNUP</a>
+					</li>
+				<?php endif; ?>
+    		</ul>
+  		</div>
+    </nav>
+    <div class="container">
+        <div class="top-details row mt-2 mb-4 d-flex">
+            <div class="col-sm-9">
+                <h4>2 BHK Flat</h4>
+                <p>800 sq.ft</p>
+                <p>K.D. Tower, Byculla, Mumbai
+            </div>
+            <div class="text-right col-sm-3 mt-2 ">
+                <h4 class="font-weight-bold mb-4">₹ 40,000</h4>
+                <a class="cn-bt">Contact Owner</a>
+            </div>
+        </div>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block img-fluid w-100" src="asset/dprop1.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block img-fluid w-100" src="asset/dprop2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block img-fluid w-100" src="asset/dprop3.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block img-fluid w-100" src="asset/dprop4.jpg" alt="Fourth slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+    </div>
+
+    
+</body>
+</html>
