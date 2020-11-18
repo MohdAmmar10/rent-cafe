d57c3bc0a987e01ef68c43b941457f5c4ce0b2fe
unknown <mohd.ammarm>
2020-11-18 18:58:46 +0530

    updated properties page

--- properties.php
+++ properties.php
@@ -44,94 +44,63 @@
   		</div>
 	</nav>
 <div class="container py-5">
-    
-
-    
     <!-- First Row [Prosucts]-->
     <h2 class="font-weight-bold mb-2">From the Shop</h2>
     <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
     
     <div class="row pb-5 mb-4">
-        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 mx-auto">
             <!-- Card-->
-            <div class="card rounded shadow-sm border-0">
-                <div class="card-body p-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556485076/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
-                    <h5> <a href="#" class="text-dark">Awesome product</a></h5>
-                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    <ul class="list-inline small">
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
-                    </ul>
+            <a class="custom-card" href="prop_details.php">
+            <div class="card rounded shadow border-0">
+                <div class="card-header"><img class="header-img" src="asset/prop1.jpg" alt="" class="img-fluid d-block mx-auto "></div>
+                <div class="card-body p-3">
+                    <h5>2 BHK Flat</h5>
+                    <p class="small text-muted font-italic">Byculla, Mumbai</p>
+                    <h5>₹ 40,000</h5>
+                    <a class="cn-bt">Contact Now</a>
                 </div>
             </div>
+            </a>
         </div>
         
         <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
             <!-- Card-->
-            <div class="card rounded shadow-sm border-0">
-                <div class="card-body p-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556485077/shoes-3_rk25rt.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
-                    <h5> <a href="#" class="text-dark">Awesome product</a></h5>
-                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    <ul class="list-inline small">
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
-                    </ul>
+            <div class="card rounded shadow border-0">
+                <div class="card-header"><img class="header-img" src="asset/prop2.jpg" alt="" class="img-fluid d-block mx-auto "></div>
+                <div class="card-body p-3">
+                    <h5>2 BHK Flat</h5>
+                    <p class="small text-muted font-italic">Colaba, Mumbai</p>
+                    <h5>₹ 60,000</h5>
+                    <a class="cn-bt">Contact Now</a>
                 </div>
             </div>
         </div>
-        
         <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
             <!-- Card-->
-            <div class="card rounded shadow-sm border-0">
-                <div class="card-body p-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556485078/shoes-2_g4qame.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
-                    <h5> <a href="#" class="text-dark">Awesome product</a></h5>
-                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    <ul class="list-inline small">
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                    </ul>
+            <div class="card rounded shadow border-0">
+                <div class="card-header"><img class="header-img" src="asset/prop3.jpg" alt="" class="img-fluid d-block mx-auto "></div>
+                <div class="card-body p-3">
+                    <h5>1 BHK Flat</h5>
+                    <p class="small text-muted font-italic">Thane, Mumbai</p>
+                    <h5>₹ 20,000</h5>
+                    <a class="cn-bt">Contact Now</a>
                 </div>
             </div>
         </div>
-        
         <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
             <!-- Card-->
-            <div class="card rounded shadow-sm border-0">
-                <div class="card-body p-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556485078/shoes-4_vgfjy9.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
-                    <h5> <a href="#" class="text-dark">Awesome product</a></h5>
-                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    <ul class="list-inline small">
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                        <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
-                    </ul>
+            <div class="card rounded shadow border-0">
+                <div class="card-header"><img class="header-img" src="asset/prop4.jpg" alt="" class="img-fluid d-block mx-auto "></div>
+                <div class="card-body p-3">
+                    <h5>1 BHK Flat</h5>
+                    <p class="small text-muted font-italic">Andheri, Mumbai</p>
+                    <h5>₹ 50,000</h5>
+                    <a class="cn-bt">Contact Now</a>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-
-
-<div class="card">
-  <div class="card-header">
-    <div class="header-img"></div>
-  </div>
-  <div class="card-title"> 
-    <h1>Breakfast Dinner</h1>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.</p><a class="btn">Show me more recipse </a>
-  </div>
 </div>
 
 </body>
